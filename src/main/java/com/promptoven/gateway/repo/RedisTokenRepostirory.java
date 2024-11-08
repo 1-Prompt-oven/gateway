7b7aa459f55833d08fc7774ea2d61ebdf68a22f6
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-08 22:10:11 +0900

    Fix: Redis Class seperate service and config to fix not registered redistemplate bean

--- src/main/java/com/promptoven/gateway/repo/RedisTokenRepostirory.java
+++ src/main/java/com/promptoven/gateway/repo/RedisTokenRepostirory.java
@@ -1,38 +1,19 @@
 package com.promptoven.gateway.repo;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @EnableRedisRepositories
+@RequiredArgsConstructor
 public class RedisTokenRepostirory {
 
-    private final RedisTemplate<String, String> redisTemplate;
-
-	public RedisTokenRepostirory(@Value("${spring.data.redis.host}") String host,
-		@Value("${spring.data.redis.port}") int port) {
-		this.redisTemplate = createRedisTemplate(host, port);
-	}
-
-	private RedisTemplate<String, String> createRedisTemplate(String host, int port) {
-		RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration(host, port);
-		RedisConnectionFactory connectionFactory = new LettuceConnectionFactory(redisConfiguration);
-
-		RedisTemplate<String, String> template = new RedisTemplate<>();
-		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new StringRedisSerializer());
-		template.setConnectionFactory(connectionFactory);
-		template.afterPropertiesSet();
-		return template;
-	}
+	private final RedisTemplate<String, String> redisTemplate;
 
 	public boolean isTokenBlocked(String token) {
-		return redisTemplate.hasKey(token);
+		return Boolean.TRUE.equals(redisTemplate.hasKey(token));
 	}
 }
