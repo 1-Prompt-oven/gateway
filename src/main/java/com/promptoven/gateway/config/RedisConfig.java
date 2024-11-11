1af57b2b1f99be6eb8e1ff3b22b0634fc8af02e1
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-11 17:25:53 +0900

    :construction: add Redis connect test

--- src/main/java/com/promptoven/gateway/config/RedisConfig.java
+++ src/main/java/com/promptoven/gateway/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.promptoven.gateway.config;
 
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +11,30 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@Slf4j
 public class RedisConfig {
 
 	@Value("${spring.data.redis.host}")
 	String host;
 	@Value("${spring.data.redis.port}")
 	int port;
+
+
+	@PostConstruct
+    public void logRedisConfig() {
+        log.info("Attempting Redis connection to {}:{}", host, port);
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(host, port), 1000);
+            socket.close();
+            log.info("TCP connection to Redis successful");
+        } catch (Exception e) {
+            log.error("TCP connection to Redis failed: {}", e.getMessage());
+        }
+    }
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
